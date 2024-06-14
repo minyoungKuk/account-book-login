04dc99ff700b4f68a2d54b0680cd4d2db2c2de38
minyoung <minyoung@minyoung.local>
2024-06-14 13:04:59 +0900

    test

--- src/api/api.ts
+++ src/api/api.ts
@@ -5,6 +5,7 @@ import ListsAPI from "./lists.api";
 const BASE_URL_AUTH = "https://moneyfulpublicpolicy.co.kr/";
 // const BASE_URL_LISTS = "https://alluring-ambitious-forest.glitch.me/account/";
 const BASE_URL_LISTS = `${import.meta.env.VITE_BASEURL}/account/`;
+console.log("123", import.meta.env.VITE_BASEURL);
 
 class API {
   auth;
